bcf4d3047e99b8b74e3deebb50293bafaa3e1d1e
Alexander Curtis <ajcurtis@usf.edu>
2024-09-08 22:09:33 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -6,3 +6,7 @@ let bill = 275;
 let totalBill = bill + tip;
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${totalBill}`);
 
+// Task 3: Create a Function
+function calculateTip(bill) { return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20};
+console.log(`Tip for a $100 bill: $${calculateTip(100)}`);
+
